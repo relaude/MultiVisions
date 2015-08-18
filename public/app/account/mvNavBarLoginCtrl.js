0a8b15a2fc1341b231b663506a120903e8f3096c
relaude <laude.ramil@gmail.com>
2015-08-18 14:23:50 +0800

    add toaster notification

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,9 +1,14 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http){
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier){
     $scope.signin = function(username, password){
         $http.post('/login', {username:username, password:password })
             .then(function(response){
-                if(response.data.success){ console.log("logged in..."); }
-                else{ console.log("failed to log in...") }
+                if(response.data.success) {
+                    mvIdentity.currentUser = response.data.user;
+                    mvNotifier.notify("Signed in successfully!");
+                }
+                else{
+                    mvNotifier.notify("Incorrect Username/Password.");
+                }
             });
     }
 });
