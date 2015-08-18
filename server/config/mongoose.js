d64983bfe3167c76af302602af62019473cc7b21
relaude <laude.ramil@gmail.com>
2015-08-18 16:28:53 +0800

    hashed password

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    crypto = require('crypto');
 
 module.exports = function(config){
     mongoose.connect(config.db);
@@ -13,16 +14,44 @@ module.exports = function(config){
     var userSchema = mongoose.Schema({
         fname: String,
         lname: String,
-        username: String
+        username: String,
+        salt: String,
+        hashed_pwd: String
     });
+
+    userSchema.methods = {
+        authenticate: function(passwordToMatch){
+            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+        }
+    }
 
     var  User = mongoose.model('User', userSchema);
 
     User.find({}).exec(function(err, collection){
         if(collection.length === 0){
-            User.create({fname:'Meg',lname:'Laude',username:'meg'});
-            User.create({fname:'Poochie',lname:'Kulit',username:'poochie'});
-            User.create({fname:'Ramil',lname:'Laude',username:'ramil'});
+            var salt, hash;
+
+            salt = createSalt();
+            hash = hashPwd(salt, 'meg');
+            User.create({fname:'Meg',lname:'Laude',username:'meg', salt: salt, hashed_pwd: hash});
+
+            salt = createSalt();
+            hash = hashPwd(salt, 'poochie');
+            User.create({fname:'Poochie',lname:'Kulit',username:'poochie', salt: salt, hashed_pwd: hash});
+
+
+            salt = createSalt();
+            hash = hashPwd(salt, 'ramil');
+            User.create({fname:'Ramil',lname:'Laude',username:'ramil', salt: salt, hashed_pwd: hash});
         }
     });
+}
+
+function createSalt(){
+    return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd){
+    var hmac = crypto.createHmac('sha1', salt);
+    return hmac.update(pwd).digest('hex');
 }
